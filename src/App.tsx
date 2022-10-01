cc400797b213643f898b4917501d03a0f4daf6d3
Farlaf <slepuhovoleg@gmail.com>
2022-10-01 10:57:40 +0500

    add routs and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,13 @@
-import { Loader } from "./components/Loader";
-import { Product } from "./components/Product";
-import { useProducts } from "./hooks/products";
-import { ErrorMessage } from "./components/Error";
-import { Modal } from "./components/Modal";
-import { CreateProduct } from "./components/CreateProduct";
-import { useContext } from "react";
-import { IProduct } from "./models";
-import { ModalContext } from "./context/ModalContext";
+import { Route, Routes } from "react-router-dom";
+import { ProductsPage } from "./pages/ProductsPage";
+import { AboutPage } from "./pages/AboutPage";
 
 function App() {
-    const { error, loading, products, addProduct } = useProducts();
-    // const [modal, setModal] = useState(true);
-    const { modal, open, close } = useContext(ModalContext);
-
-    const createHandler = (product: IProduct) => {
-        close();
-        addProduct(product);
-    };
-
     return (
-        <div className="container mx-auto max-w-2xl pt-5">
-            {error && <ErrorMessage message={error} />}
-            {loading && <Loader />}
-            {products.map((product) => (
-                <Product key={product.id} product={product} />
-            ))}
-
-            {modal && (
-                <Modal title="Create new product" onClose={close}>
-                    <CreateProduct onCreate={createHandler} />
-                </Modal>
-            )}
-
-            <button
-                className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"
-                onClick={open}
-            >
-                +
-            </button>
-        </div>
+        <Routes>
+            <Route path="/" element={<ProductsPage />} />
+            <Route path="/about" element={<AboutPage />} />
+        </Routes>
     );
 }
 
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function AboutPage() {
+    return (
+        <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
+            facere repellat magnam ducimus ab porro consequuntur at molestias
+            corrupti reprehenderit laboriosam enim sunt, dolore laborum optio
+            fuga maiores. Laboriosam autem aliquid eius illum nulla. Quas,
+            mollitia quibusdam quidem, architecto, expedita repellendus minima
+            deserunt voluptatibus non quam commodi sunt incidunt! Cupiditate.
+        </div>
+    );
+}
