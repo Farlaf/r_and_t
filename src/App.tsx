f7e01c6dfe2db16a7f23cab3c986b492afbed2e7
Farlaf <slepuhovoleg@gmail.com>
2022-10-01 10:36:50 +0500

    add context for popup

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import { useProducts } from "./hooks/products";
 import { ErrorMessage } from "./components/Error";
 import { Modal } from "./components/Modal";
 import { CreateProduct } from "./components/CreateProduct";
-import { useState } from "react";
+import { useContext } from "react";
 import { IProduct } from "./models";
+import { ModalContext } from "./context/ModalContext";
 
 function App() {
     const { error, loading, products, addProduct } = useProducts();
-    const [modal, setModal] = useState(true);
+    // const [modal, setModal] = useState(true);
+    const { modal, open, close } = useContext(ModalContext);
 
     const createHandler = (product: IProduct) => {
-        setModal(false);
+        close();
         addProduct(product);
     };
 
@@ -25,17 +27,14 @@ function App() {
             ))}
 
             {modal && (
-                <Modal
-                    title="Create new product"
-                    onClose={() => setModal(false)}
-                >
+                <Modal title="Create new product" onClose={close}>
                     <CreateProduct onCreate={createHandler} />
                 </Modal>
             )}
 
             <button
                 className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"
-                onClick={() => setModal(true)}
+                onClick={open}
             >
                 +
             </button>
